c5f32106dedccf499176b32e29fb31d7b42b7e82
Lorenzo Romero <loroji11@gmail.com>
2026-03-20 00:57:18 +0100

    All course done, just need to make montecarlo app simulation

---
+++ montecarlo_simulation_app.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yfi
+from scipy.stats import norm
+
+class Monte_Carlo:
+    '''
+        Montecarlo class
+    '''
+    
